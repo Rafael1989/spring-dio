9c9e202eb1dd2f9b9803b7ac14c9e27231d41a1d
Rafael <flns_rafa@hotmail.com>
2022-08-24 18:07:46 +0200

    Spring

---
+++ springboot-ioc-di-beans-autowired/src/test/java/dio/springbootiocdibeansautowired/SpringbootIocDiBeansAutowiredApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.springbootiocdibeansautowired;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootIocDiBeansAutowiredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
